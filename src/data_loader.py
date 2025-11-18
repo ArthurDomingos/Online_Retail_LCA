43c4350f5c1e693f25201dab245bf5055053c378
Arthur Maia Domingos <79126362+ArthurDomingos@users.noreply.github.com>
2025-11-18 09:06:02 -0300

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,6 @@
-
+def load_data():
+    from ucimlrepo import fetch_ucirepo
+    online_retail = fetch_ucirepo(id=352)
+    df = online_retail.data.features.reset_index()
+    df = df.rename(columns={'index':'InvoiceNo'})
+    return df
